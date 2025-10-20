137e7c5bdc50ece0ccce5d726a2174c715e08f94
Ashish <ashuk5479@gmail.com>
2025-10-20 19:37:10 +0530

    jokes using api

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
 import './App.css'
-import LudoBoard from './LudoBoard';
-import TodoList from './TodoList';
-import Lottery from './Lottery';
-import TicketNum from './TicketNum';
-import Ticket from './Ticket';
-import { sum } from './helper';
-import Form from './Form';
-import CommentsForm from './CommentsForm';
-import Comment from "./Comment";
-import Counter from './Counter';
+import Joker from './Joker';
 
  function App() {
-  // let winCondition = (ticket) => {
-  //   return sum(ticket) === 15;
-  // };
 
   return (
     <>
-{/* <Lottery  n={3} winCondition={winCondition}></Lottery>*/}
-{/* <Form></Form> */}
- {/* <CommentsForm></CommentsForm>  */}
-{/* <Comment></Comment> */}
-<Counter></Counter>
+<Joker></Joker>
     </>
   );
 };
